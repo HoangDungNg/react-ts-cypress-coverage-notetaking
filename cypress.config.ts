55c0f0c5edcac06bbede74b8e9df728a64f26f04
Hoang Dung <dungnguyen322003@gmail.com>
2023-05-24 09:30:26 +0700

    add coverage to cypress config, remove babel core

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,15 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
+import codeCoverageTask from '@cypress/code-coverage/task';
+import vitePreprocessor from 'cypress-vite';
 
 export default defineConfig({
   e2e: {
+    baseUrl: 'http://localhost:3000/',
+    supportFile: 'cypress/support/e2e.ts',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on('file:preprocessor', vitePreprocessor());
+      codeCoverageTask(on, config);
+      return config;
     },
   },
 });
